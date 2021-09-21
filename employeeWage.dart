4bee58081bf49d358b73f36d6c87ce9526dfd219
Alternade <aptsahu2@gmail.com>
2021-09-21 10:58:00 +0530

    Added daily wage

--- employeeWage.dart
+++ employeeWage.dart
@@ -1,10 +1,14 @@
 import 'dart:math';
 
 void main() {
+  int wagePerHr = 20;
+  int workHour = 8;
   Random rng = new Random();
   int randomNumber = rng.nextInt(2);
   if (randomNumber == 1) {
     print("Employee is present");
+    int wage = wagePerHr * workHour;
+    print("Daily Wage = $wage");
   } else {
     print("Employee is absent");
   }
