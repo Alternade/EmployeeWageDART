0d49918d30c2791060513bd7fa82bcd5e1347459
Alternade <aptsahu2@gmail.com>
2021-09-21 11:23:32 +0530

    Added Commit

--- employeeWage.dart
+++ employeeWage.dart
@@ -4,20 +4,33 @@ void main() {
   int wagePerHr = 20;
   Random rng = new Random();
   int randomNumber = rng.nextInt(3);
-  if (randomNumber == 1) {
-    print("Employee is present");
-    int workHour = 8;
-    int wage = wagePerHr * workHour;
-    print("Daily Wage = $wage");
-  } else if (randomNumber == 2) {
-    print("Employee is present for part time");
-    int workHour = 4;
-    int wage = wagePerHr * workHour;
-    print("Daily Wage = $wage");
-  } else {
-    print("Employee is absent");
-    int workHour = 0;
-    int wage = wagePerHr * workHour;
-    print("Daily Wage = $wage");
+  int workHr = 0;
+  switch (randomNumber) {
+    case 1:
+      {
+        print("Employee is Present");
+        int workHr = 8;
+        int sal = wagePerHr * workHr;
+        print("Salary = $sal");
+      }
+      break;
+
+    case 2:
+      {
+        print("Employee is part time");
+        int workHr = 4;
+        int sal = wagePerHr * workHr;
+        print("Salary = $sal");
+      }
+      break;
+
+    default:
+      {
+        print("Employee is absent");
+        int workHr = 0;
+        int sal = wagePerHr * workHr;
+        print("Salary = $sal");
+      }
+      break;
   }
 }
