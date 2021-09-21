5685069b39a25af36305d6534a928bc25befd06f
Alternade <aptsahu2@gmail.com>
2021-09-21 11:07:15 +0530

    Added Part Time

--- employeeWage.dart
+++ employeeWage.dart
@@ -2,14 +2,22 @@ import 'dart:math';
 
 void main() {
   int wagePerHr = 20;
-  int workHour = 8;
   Random rng = new Random();
-  int randomNumber = rng.nextInt(2);
+  int randomNumber = rng.nextInt(3);
   if (randomNumber == 1) {
     print("Employee is present");
+    int workHour = 8;
+    int wage = wagePerHr * workHour;
+    print("Daily Wage = $wage");
+  } else if (randomNumber == 2) {
+    print("Employee is present for part time");
+    int workHour = 4;
     int wage = wagePerHr * workHour;
     print("Daily Wage = $wage");
   } else {
     print("Employee is absent");
+    int workHour = 0;
+    int wage = wagePerHr * workHour;
+    print("Daily Wage = $wage");
   }
 }
